ddad98b2cff5b84c125ce60e4e35c9bee227ed73
Shaalan15 <shaalan_a@outlook.com>
2024-11-09 20:34:18 +0400

    updated skeleton and game state

--- hoodid/models/room.py
+++ hoodid/models/room.py
@@ -1,6 +1,7 @@
+from hoodid.models.card import Card
 from hoodid.models.location import Location
 
-class Room(Location):
+class Room(Location, Card):
     
     def __init__(self) -> None:
         super().__init__()
