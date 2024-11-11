78d088d23b6fba8c7eec5a94b93b12b5b331aaa0
Shaalan15 <shaalan_a@outlook.com>
2024-11-12 02:08:26 +0400

    switched to an empty gamestate

--- frontend/src/app/game.state.service.ts
+++ frontend/src/app/game.state.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal, Signal } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 import { GameState } from '../models/game.state.model';
 
 @Injectable({
@@ -9,54 +9,13 @@ export class GameStateService {
   constructor() { }
 
   gameState = signal<GameState>({
-    character: [
-      {
-        name: "Professor Plum",
-        location: {
-          name: "Kitchen",
-          locationType: "Room",
-          connectedLocations: [
-            {
-              name: "Kitchen",
-              locationType: "Room",
-              connectedLocations: [],
-              occupied: true,
-              weapon: {
-                name: "Wrench"
-              }
-            },
-            {
-              name: "Kitchen",
-              locationType: "Room",
-              connectedLocations: [],
-              occupied: true,
-              weapon: {
-                name: "Wrench"
-              }
-            }
-          ],
-          occupied: true,
-          weapon: {
-            name: "Wrench"
-          }
-        },
-        homeSquare: {
-          name: "Kitchen",
-          locationType: "Room",
-          connectedLocations: [],
-          occupied: true,
-          weapon: {
-            name: "Wrench"
-          }
-        }
-      }
-    ],
-    currentTurn: "Professor Plum",
+    characters: [],
+    currentTurn: "",
     lastActionTaken: {
-      type: "Action",
-      character: "Professor Plum",
-      location: "Kitchen",
-      message: "Professor Plum moved to Kitchen."
+      type: "",
+      character: "",
+      location: "",
+      message: ""
     }
   })
 }
